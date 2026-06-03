d5595fb08bc9fc1e114f452a7fa9d612b38c2373
ARUMUGAM K <suriyakannan098@gmail.com>
2026-06-03 21:24:19 +0530

    Delete MODULE-3/Java directory

--- MODULE-3/Java/Exercise34/com.greetings/module-info.java
+++
@@ -1,4 +0,0 @@
-module com.greetings {
-    // Declare a dependency on the com.utils module
-    requires com.utils;
-}
--- MODULE-3/Java/Exercise34/com.utils/module-info.java
+++
@@ -1,4 +0,0 @@
-module com.utils {
-    // Export the com.utils package so other modules can use the Utility class
-    exports com.utils;
-}
